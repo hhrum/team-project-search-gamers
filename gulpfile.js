0179fde86c883611f7404535b9bc4f4d91fbe876
OrtemkaDead <temakravchenko2004@gmail.com>
2020-12-25 15:03:13 +0300

    Переделал по заказу homepage

--- gulpfile.js
+++ gulpfile.js
@@ -117,7 +117,7 @@ function images() {
             progressive: true,
             svgoPlugins: [{ removeViewBox: false }],
             interlaced: true,
-            optimizationLevel: 3 // 0 to 7
+            optimizationLevel: 1 // 0 to 7
          })
       )
       .pipe(dest(path.build.img))
