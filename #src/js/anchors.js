8bcf6ed433746c5c893030ee77984ce8c143c672
OrtemkaDead <temakravchenko2004@gmail.com>
2020-12-26 23:03:39 +0300

    Свайпы

--- #src/js/anchors.js
+++ #src/js/anchors.js
@@ -13,8 +13,6 @@
 // }
 
 var wrapper = document.querySelector('.liked');
-var btnNext = document.querySelector(".liked__btn-next");
-var btnPrev = document.querySelector(".liked__btn-prev");
 var elementWidth = document.querySelector('.liked__item').clientWidth;
 var i = 0;
 const items = document.querySelectorAll(".liked__item");
@@ -28,17 +26,37 @@ function scrollTo(e) {
    })
 }
 
-btnNext.onclick = () => {
-   if (i < items.length - 1) {
-      i++;
-      scrollTo((elementWidth + 24) * i);
-   }
-}
 
-btnPrev.onclick = () => {
-   if (i == 0) { }
-   else {
-      i--;
-      scrollTo((elementWidth + 24) * i);
+var initialPoint;
+var finalPoint;
+document.addEventListener('touchstart', function (event) {
+   event.preventDefault();
+   event.stopPropagation();
+   initialPoint = event.changedTouches[0];
+}, false);
+document.addEventListener('touchend', function (event) {
+   event.preventDefault();
+   event.stopPropagation();
+   finalPoint = event.changedTouches[0];
+   var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+   var yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
+   if (xAbs > 20 || yAbs > 20) {
+      if (xAbs > yAbs) {
+         if (finalPoint.pageX < initialPoint.pageX) {
+            /*СВАЙП ВЛЕВО*/
+            if (i < items.length - 1) {
+               i++;
+               scrollTo((elementWidth + 24) * i);
+            }
+         }
+         else {
+            /*СВАЙП ВПРАВО*/
+            if (i == 0) { }
+            else {
+               i--;
+               scrollTo((elementWidth + 24) * i);
+            }
+         }
+      }
    }
-}
+}, false);
